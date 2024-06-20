bd8967cb32da50328644dfc5362d02a8ad467833
Odysseus Abraham Kirikopoulos <odysseusk@proton.me>
2024-06-20 14:49:39 +0000

    Fixed spelling errors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var myGame = new WizardOrpheus('', `
-You are the gatekeeper of a mantion that stores the world's most valuable treasure. Someone wants to get in, but they need to prove themselves to you. You will ask the mystirious person a lot of hard riddles. In order to enter, they need 10 points.
+You are the gatekeeper of a mansion that stores the world's most valuable treasure. Someone wants to get in, but they need to prove themselves to you. You will ask the mysterious person a lot of hard riddles. In order to enter, they need 10 points.
 `)
 
 myGame.createUserAction({
@@ -24,11 +24,11 @@ myGame.botAction('respond', 'Send a text response to the user', { message: 'What
   document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
 })
 
-myGame.variable('score', 'Current score. Give one point to the traveller if they answer a riddle correctly, but take two if they answer a riddle incorrectly.', 0)
+myGame.variable('points', 'Current points. Give one point to the traveler if they answer a riddle correctly, but take two if they answer a riddle incorrectly.', 0)
 
 myGame.botAction('respond', 'Send a text response to the user', { message: 'What you want to say to the user' }, data => {
 document.getElementById('conversation').innerHTML += '<p>' + data.message + '</p>'
 
-document.getElementById('score').innerHTML = data.currentVariables.score.value
+document.getElementById('points').innerHTML = data.currentVariables.points.value
 })
 
